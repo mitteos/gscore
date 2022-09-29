4df085b3e1a0863925fff6432277373a37e22e8f
Dmitry <90985199+mitteos@users.noreply.github.com>
2022-09-29 14:32:40 +0300

    fix import paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import "../styles/normalize.css"
-import "../styles/globals.css"
+import "styles/normalize.css"
+import "styles/globals.css"
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {

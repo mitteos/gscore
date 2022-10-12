0e74c1d654870bbc2f13b1ab1cec7b77d6edbe3b
Dmitry <90985199+mitteos@users.noreply.github.com>
2022-10-12 18:36:48 +0300

    create redux store, connect redux-persist; create axios queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import "styles/normalize.css"
 import "styles/fonts.css"
 import "styles/globals.css"
 import type { AppProps } from "next/app"
+import { Provider } from "react-redux"
+import { persistor, store } from "store/store"
+import { PersistGate } from "redux-persist/integration/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Component {...pageProps} />
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<Component {...pageProps} />
+			</PersistGate>
+		</Provider>
 	)
 }
 
